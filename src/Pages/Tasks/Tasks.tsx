4575d57dcdcb39830a5d766c20d47494dac79ead
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-18 16:10:28 +0600

    create “Selector Functions” for better modularity

--- src/Pages/Tasks/Tasks.tsx
+++ src/Pages/Tasks/Tasks.tsx
@@ -1,6 +1,12 @@
+import { selectFilter, selectTasks } from "@/redux/features/task/taskSlice";
+import { useAppSelector } from "@/redux/hook";
 
 
 const Tasks = () => {
+    const tasks =useAppSelector(selectTasks)
+    const filter =useAppSelector(selectFilter)
+    console.log(tasks);
+    console.log(filter);
     return (
         <div>
              <h1 className="text-3xl h-screen flex justify-center items-center">This is Tasks page</h1>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,12 +1,14 @@
+import { RootState } from "@/redux/store";
 import { ITask } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 
 interface InitialState{
-    task:ITask[]
+    tasks:ITask[],
+    filter:"all"|"high"|"medium"|"low";
 }
 
 const initialState:InitialState={
-    task:[
+    tasks:[
         {
             id: "asdfghjks",
             title: "Complete Redux Setup",
@@ -16,11 +18,18 @@ const initialState:InitialState={
             priority: "high",
             
         }
-    ]
+    ],
+    filter:"all"
 }
 const taskSlice = createSlice({
     name:"task",
     initialState,
     reducers:{},
 })
+export const selectTasks = (state:RootState)=>{
+    return state.todo.tasks;
+}
+export const selectFilter = (state:RootState)=>{
+    return state.todo.filter;
+}
 export default taskSlice.reducer;
