2f180f4a4333949c346fe9438f44632319a4a2d1
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-18 21:09:12 +0600

    Create modal using shadcn

--- src/Pages/Tasks/Tasks.tsx
+++ src/Pages/Tasks/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/module/AddTaskModal/AddTaskModal";
 import TaskCard from "@/module/Task/taskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -12,9 +13,9 @@ const Tasks = () => {
     <h1 className="text-3xl flex justify-center items-center">
           Tasks List
         </h1>
-     
-  <div className="mx-auto max-w-7xl px-5 mt-10">
     
+  <div className="mx-auto max-w-7xl px-5 mt-10">
+    <div className="flex justify-end"> <AddTaskModal /></div>
       <div className="space-y-5 mt-5">
      {
         tasks?.map(task=><TaskCard task={task}/>)
