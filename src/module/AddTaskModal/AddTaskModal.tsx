b59d2e26a9e21703329ece5f4e347378c19b4f80
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-01-20 16:10:27 +0600

    Building the Add Task Form

--- src/module/AddTaskModal/AddTaskModal.tsx
+++ src/module/AddTaskModal/AddTaskModal.tsx
@@ -13,8 +13,12 @@ import {
 } from "@/components/ui/dialog"
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Select, SelectContent,  SelectItem,  SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form"
 
 
@@ -62,47 +66,71 @@ export function AddTaskModal() {
       </FormItem>
     )}
   />
- <FormField
-    control={form.control}
-    name="Due Date"
-    render={({field}) => (
-      <FormItem>
-        <FormLabel>Deu Date</FormLabel>
-        <FormControl>
-          <Calendar {...field} />
-        </FormControl>
+   <FormField
+          control={form.control}
+          name="dueDate"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Due Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        " pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    // disabled={(date) =>
+                    //   date > new Date() || date < new Date("1900-01-01")
+                    // }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
       
-      </FormItem>
-    )}
-  />
- <FormField
-    control={form.control}
-    name="Priority"
-    render={({field}) => (
-      <FormItem>
-        <FormLabel>Priority</FormLabel>
-        <FormControl>
-        <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select priority" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="high">High</SelectItem>
-          <SelectItem value="medium">Medium</SelectItem>
-          <SelectItem value="low">Low</SelectItem>
-          
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-        </FormControl>
-      
-      </FormItem>
-    )}
-  />
+            </FormItem>
+          )}
+        />
+  <FormField
+          control={form.control}
+          name="priority"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Priority</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a Priority to set" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="high">High</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="low">Low</SelectItem>
+                </SelectContent>
+              </Select>
+        
+            </FormItem>
+          )}
+        />
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button className="mt-5" type="submit">Save changes</Button>
         </DialogFooter>
  </form>
      </Form>
